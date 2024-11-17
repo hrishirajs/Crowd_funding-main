8529420a94f65d2238a50bdd58ebbc9f6beb05dc
hrishirajs <135799731+hrishirajs@users.noreply.github.com>
2024-11-18 00:46:10 +0530

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -4,6 +4,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   connectTimeoutMS: 30000, // 30 seconds timeout
+  ssl: true,
+  tlsAllowInvalidCertificates: false,
 });
 
 
