e8d816211219f9e2e9eaba16632d3db34c529bd8
hrishirajs <135799731+hrishirajs@users.noreply.github.com>
2024-11-18 00:11:55 +0530

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,21 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000, // 30 seconds timeout
+});
 
-mongoose
-  .connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(`MongoDB connection error : ${err}`));
+
+
 
 module.exports = {
   Campaign: require("./Campaign"),
