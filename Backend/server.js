bcdcd6636de3f8e32766a77b050422650308041c
hrishirajs <135799731+hrishirajs@users.noreply.github.com>
2024-11-17 22:28:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // -----ENV Setup----- //
 require("dotenv").config();
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 const routes = require("./routes");
 
 // -----Middleware----- /
